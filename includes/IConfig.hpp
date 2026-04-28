c1b11883e6f1db83978f85af4b362627799dbea0
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-04-28 11:40:33 -0300

    feat: complete access methods

--- includes/IConfig.hpp
+++ includes/IConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 09:43:59 by mhidani           #+#    #+#             */
-/*   Updated: 2026/04/27 18:25:39 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/04/28 10:37:16 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ class IConfig {
 public:
 	virtual			~IConfig(void) {};
 	virtual int		port(void) const = 0;
+	virtual void	port(const int& port) = 0;
 	virtual int		addrIPv(void) const = 0;
+	virtual void	addrIPv(const int& addrIPv) = 0;
 	virtual int		tpn(void) const = 0;
+	virtual void	tpn(const int& tpn) = 0;
 };
