c5f369d3552873cb7f211ffe73e8ece3c4b195df
joaolive <devjoaolive@outlook.com>
2026-04-30 16:48:49 -0300

    feat(location): add LocationConfig class with method handling and routing settings

---
+++ include/locationConfig.hpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   locationConfig.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaolive <joaolive@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/28 17:57:52 by joaolive          #+#    #+#             */
+/*   Updated: 2026/04/30 19:45:54 by joaolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <cstdint>
+#include <map>
+#include <vector>
+
+enum HttpMethod {
+	METHOD_NONE		= 0,
+	METHOD_GET		= 1 << 0,
+	METHOD_POST		= 1 << 1,
+	METHOD_PUT		= 1 << 2,
+	METHOD_DELETE	= 1 << 3,
+	METHOD_PATCH	= 1 << 4
+};
+
+class LocationConfig {
+	private:
+		std::string							_path;
+		std::string							_alias;
+		std::string							_root;
+		bool								_autoindex;
+		uint8_t								_allow_methods;
+		uint64_t							_client_max_body_size;
+		std::vector<std::string>			_index;
+		std::vector<std::string>			_cgi_path;
+		std::vector<std::string>			_cgi_ext;
+		std::pair<uint16_t, std::string>	_return;
+		std::map<uint16_t, std::string>		_error_pages;
+	public:
+		LocationConfig();
+		LocationConfig(const LocationConfig& rhs);
+		LocationConfig& operator=(const LocationConfig& rhs);
+		~LocationConfig();
+
+		const std::string& getPath() const;
+		const std::string& getAlias() const;
+		const std::string& getRoot() const;
+		bool getAutoindex() const;
+		uint8_t getAllowMethods() const;
+		uint64_t getClientMaxBodySize() const;
+		const std::vector<std::string>& getIndex() const;
+		const std::vector<std::string>& getCgiPath() const;
+		const std::vector<std::string>& getCgiExt() const;
+		const std::pair<uint16_t, std::string>& getReturn() const;
+		const std::map<uint16_t, std::string>& getErrorPages() const;
+
+		void setPath(const std::string& path);
+		void setAlias(const std::string& alias);
+		void setRoot(const std::string& root);
+		void setAutoindex(bool autoindex);
+		void setReturn(uint16_t code, const std::string& url);
+
+		void addIndex(const std::string& index);
+		void addCgiPath(const std::string& path);
+		void addCgiExt(const std::string& ext);
+		void addMethod(uint8_t method);
+		void addErrorPage(uint16_t key, const std::string& value);
+
+		bool isMethodAllowed(uint8_t method) const;
+};
---
+++ src/config/locationConfig.cpp
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   locationConfig.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaolive <joaolive@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/28 18:04:19 by joaolive          #+#    #+#             */
+/*   Updated: 2026/04/30 19:45:18 by joaolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "locationConfig.hpp"
+
+LocationConfig::LocationConfig() 
+	: _path(""), 
+	  _alias(""), 
+	  _root(""), 
+	  _autoindex(false), 
+	  _allow_methods(0), 
+	  _client_max_body_size(0),
+	  _return(std::make_pair(0, "")) {}
+
+LocationConfig::LocationConfig(const LocationConfig& rhs) {
+	*this = rhs;
+}
+
+LocationConfig& LocationConfig::operator=(const LocationConfig& rhs) {
+	if (this != &rhs) {
+		_path = rhs._path;
+		_alias = rhs._alias;
+		_root = rhs._root;
+		_autoindex = rhs._autoindex;
+		_allow_methods = rhs._allow_methods;
+		_client_max_body_size = rhs._client_max_body_size;
+		_index = rhs._index;
+		_cgi_path = rhs._cgi_path;
+		_cgi_ext = rhs._cgi_ext;
+		_return = rhs._return;
+		_error_pages = rhs._error_pages;
+	}
+	return (*this);
+}
+
+LocationConfig::~LocationConfig() {}
+
+// getters
+
+const std::string& LocationConfig::getPath() const {
+	return (_path);
+}
+
+const std::string& LocationConfig::getAlias() const {
+	return (_alias);
+}
+
+const std::string& LocationConfig::getRoot() const {
+	return (_root);
+}
+
+bool LocationConfig::getAutoindex() const {
+	return (_autoindex);
+}
+
+uint8_t LocationConfig::getAllowMethods() const {
+	return (_allow_methods);
+}
+
+uint64_t LocationConfig::getClientMaxBodySize() const {
+	return (_client_max_body_size);
+}
+
+const std::vector<std::string>& LocationConfig::getIndex() const {
+	return (_index);
+}
+
+const std::vector<std::string>& LocationConfig::getCgiPath() const {
+	return (_cgi_path);
+}
+
+const std::vector<std::string>& LocationConfig::getCgiExt() const {
+	return (_cgi_ext);
+}
+
+const std::pair<uint16_t, std::string>& LocationConfig::getReturn() const {
+	return (_return);
+}
+
+const std::map<uint16_t, std::string>& LocationConfig::getErrorPages() const {
+	return (_error_pages);
+}
+
+// setters
+
+void LocationConfig::setPath(const std::string& path) {
+	_path = path;
+}
+
+void LocationConfig::setAlias(const std::string& alias) {
+	_alias = alias;
+}
+
+void LocationConfig::setRoot(const std::string& root) {
+	_root = root;
+}
+
+void LocationConfig::setAutoindex(bool autoindex) {
+	_autoindex = autoindex;
+}
+
+void LocationConfig::setReturn(uint16_t code, const std::string& url) {
+	_return = std::make_pair(code, url); // C++98 way
+}
+
+// adders
+
+void LocationConfig::addIndex(const std::string& index) {
+	_index.push_back(index);
+}
+
+void LocationConfig::addCgiPath(const std::string& path) {
+	_cgi_path.push_back(path);
+}
+
+void LocationConfig::addCgiExt(const std::string& ext) {
+	_cgi_ext.push_back(ext);
+}
+
+void LocationConfig::addMethod(uint8_t method) {
+	_allow_methods |= method;
+}
+
+void LocationConfig::addErrorPage(uint16_t key, const std::string& value) {
+	_error_pages[key] = value;
+}
+
+// helpers
+
+bool LocationConfig::isMethodAllowed(uint8_t method) const {
+	return ((_allow_methods & method) != 0);
+}
