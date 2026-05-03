c95ec83128b4336ef5dc489eadd3046b422b63d6
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-05-03 09:35:01 -0300

    refact: move to infra

---
+++ include/infra/AcceptHandler.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AcceptHandler.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 18:12:36 by mhidani           #+#    #+#             */
+/*   Updated: 2026/05/03 09:15:13 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <cstring>
+#include <stdexcept>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include "IEventHandler.hpp"
+#include "ClientHandler.hpp"
+#include "contracts/IHttpProcessorFactory.hpp"
+
+class ServerEngine;
+
+class AcceptHandler : public IEventHandler {
+	private:
+		int						_fd;
+		ServerEngine*			_server;
+		IHttpProcessorFactory&	_factory;
+	protected:
+		void prepareClient(int& fd);
+	public:
+		AcceptHandler(const int& fd, 
+					  ServerEngine* sv, 
+					  IHttpProcessorFactory& fc);
+		virtual	~AcceptHandler(void);
+
+		void event(epoll_event& event);
+};
