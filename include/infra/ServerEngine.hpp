24ecdf304b7d16532b6caa5b83b03a55f9f75731
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-05-03 09:30:04 -0300

    refact: rename server engine

---
+++ include/infra/ServerEngine.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerEngine.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/01 19:21:08 by mhidani           #+#    #+#             */
+/*   Updated: 2026/05/03 09:26:50 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "IEventHandler.hpp"
+#include "AcceptHandler.hpp"
+#include "config/model/ServerConfig.hpp"
+#include "contracts/IHttpProcessorFactory.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <map>
+
+#ifndef IO_MONITOR_SIZE
+# define IO_MONITOR_SIZE 2048
+#endif
+
+#ifndef STREAMING_BUFFER_SIZE
+# define STREAMING_BUFFER_SIZE 8
+#endif
+
+
+class ServerEngine {
+	private:
+		int								_socketFd;
+		int								_ioMonitorFd;
+		std::map<int, IEventHandler*>	_handlers;
+		ServerConfig*					_config;
+	protected:
+		int createServer(void);
+		int createIoMonitor(void);
+		void startEventLoop(void);
+	public:
+		ServerEngine(ServerConfig* config);
+		virtual ~ServerEngine(void);
+
+		static int start(ServerConfig* config, IHttpProcessorFactory& factory);
+
+		int getSocketFd(void) const;
+		int getIoMonitorFd(void) const;
+		ServerConfig* getConfig(void) const;
+		IEventHandler* getHandler(const int& fd);
+		void addHandler(const int& fd, const uint32_t& events, IEventHandler* hdl);
+		void removeHandler(const int& fd);
+		void changeHandlerState(const int& fd, const uint32_t& state);
+};
---
+++ src/infra/ServerEngine.cpp
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerEngine.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/01 19:33:08 by mhidani           #+#    #+#             */
+/*   Updated: 2026/05/03 09:26:04 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ServerEngine.hpp"
+
+ServerEngine::ServerEngine(ServerConfig* config)
+	:	_config(config),  
+		_socketFd(createServer()),
+		_ioMonitorFd(createIoMonitor()) {
+}
+
+ServerEngine::~ServerEngine(void) {
+	close(_socketFd);
+	close(_ioMonitorFd);
+}
+
+int ServerEngine::createServer(void) {
+	int					fd, opt, flags;
+	struct sockaddr_in	addr;
+
+	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		throw std::runtime_error("socket"); // TODO: create exception
+	if (setsockopt(_socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+		throw std::runtime_error("setsockopt"); // TODO: create exception
+
+	addr.sin_family = AF_INET;
+	addr.sin_family = htonl(_config->getHost());
+	addr.sin_port = htons(_config->getPort());
+	flags = fcntl(fd, F_GETFL, 0);
+
+	if (bind(_socketFd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0)
+		throw std::runtime_error("bind"); // TODO: create exception
+	if (listen(fd, SOMAXCONN) < 0) // TODO: check SOMAXCONN to backlog conn
+		throw std::runtime_error("listen"); // TODO: create exception
+	if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) )
+		throw std::runtime_error("fcntl O_NONBLOCK");
+	return fd;
+}
+
+int ServerEngine::createIoMonitor(void) {
+	int	fd = 0;
+
+	if ((fd = epoll_create1(0)) < 0) {
+		close(_socketFd);
+		throw std::runtime_error("epoll_create1"); // TODO: create exception
+	}
+	return fd;
+}
+
+void ServerEngine::startEventLoop(void) {
+	epoll_event		events[IO_MONITOR_SIZE];
+	int				nEvents = 0;
+	int				fd = 0;
+
+	while (true) {
+		nEvents = epoll_wait(_ioMonitorFd, events, IO_MONITOR_SIZE, -1);
+		if (nEvents < 0)
+			throw std::runtime_error("epoll_wait"); // TODO: create exception
+
+		for (int i = 0; i < nEvents; i++) {
+			fd = events[i].data.fd;
+			_handlers[fd]->event(events[i]);
+		}
+	}
+}
+
+int ServerEngine::start(ServerConfig* config, IHttpProcessorFactory& factory) {
+	try {
+		ServerEngine	server(config);
+		int				fd = server.getSocketFd();
+		AcceptHandler*	acceptHandler = new AcceptHandler(fd, &server, factory); 
+		
+		server.addHandler(fd, EPOLLIN, acceptHandler);
+		server.startEventLoop();
+
+	} catch (const std::exception& e) {
+		std::cerr << "error: " << e.what() << std::endl; // TODO: create log capture
+		return -1;
+	}
+	return 0;
+}
+
+int ServerEngine::getSocketFd(void) const {
+	return _socketFd;
+}
+
+int ServerEngine::getIoMonitorFd(void) const {
+	return _ioMonitorFd;
+}
+
+ServerConfig* ServerEngine::getConfig(void) const {
+	return _config;
+}
+
+IEventHandler* ServerEngine::getHandler(const int& fd) {
+	return _handlers[fd];
+}
+
+void ServerEngine::addHandler(const int& fd, const uint32_t& events, IEventHandler* hdl) {
+	struct epoll_event	ev;
+
+	ev.events = events;
+	ev.data.fd = fd;
+
+	if (epoll_ctl(_ioMonitorFd, EPOLL_CTL_ADD, fd, &ev) == -1)
+		throw std::runtime_error("epoll_ctl: add"); // TODO: create exception
+	_handlers[fd] = hdl;
+}
+
+void ServerEngine::removeHandler(const int& fd) {
+	if (epoll_ctl(_ioMonitorFd, EPOLL_CTL_DEL, fd, NULL) == -1)
+		throw std::runtime_error("epoll_ctl: remove"); // TODO: create exception
+
+	delete _handlers[fd];
+	_handlers.erase(fd);
+	close(fd);
+}
+
+void ServerEngine::changeHandlerState(const int& fd, const uint32_t& state) {
+	struct epoll_event	ev;
+
+	ev.events = state;
+	ev.data.fd = fd;
+	
+	if (epoll_ctl(_ioMonitorFd, EPOLL_CTL_MOD, fd, &ev) < 0)
+		throw std::runtime_error("epoll_ctl: change"); // TODO: create exception
+}
