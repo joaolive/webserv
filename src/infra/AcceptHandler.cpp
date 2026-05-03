5ed1883bda49ddf9af186b49cd29cf6155d2d748
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-05-03 09:31:33 -0300

    feat: client identification

--- src/infra/AcceptHandler.cpp
+++ src/infra/AcceptHandler.cpp
@@ -6,35 +6,48 @@
 /*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 13:54:52 by mhidani           #+#    #+#             */
-/*   Updated: 2026/04/27 18:20:33 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/05/03 09:17:43 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AcceptHandler.hpp"
 
-AcceptHandler::AcceptHandler(Reactor* eventLoop, IHttpServer* http) {
-	_eventLoop = eventLoop;
-	_http = http;
+#include "infra/ServerEngine.hpp"
+
+AcceptHandler::AcceptHandler(const int& fd, 
+							 ServerEngine* sv, 
+							 IHttpProcessorFactory& fc)
+	:	_fd(fd), 
+		_server(sv), 
+		_factory(fc) {
 }
 
 AcceptHandler::~AcceptHandler(void) {
 }
 
-void AcceptHandler::handleEvent(epoll_event& event) {
-	sockaddr_in		addr;
-	ClientHandler*	hanlder = NULL;
-	socklen_t		addrLen = sizeof(addr);
-	int				fd = accept(_eventLoop->servFd(), 
-								reinterpret_cast<sockaddr*>(&addr), 
-								&addrLen);
+void AcceptHandler::event(epoll_event& event) {
+	struct sockaddr_in	addr;
+	socklen_t			addrLen = sizeof(addr);
+	IEventHandler*		client = NULL;
+	int					fd = 0, flags = 0;
+	uint16_t			port = 0;
+	std::string			ip = NULL;
 
-	if (fd < 0) {
+	if ((fd = accept(_server->getSocketFd(), reinterpret_cast<sockaddr*>(&addr), &addrLen)) < 0) {
 		std::cerr << "accept" << std::endl; // TODO: insert in the log
-		return;
+		return ;
 	}
-	
-	fcntl(fd, F_SETFL, O_NONBLOCK);
-	hanlder = new ClientHandler(fd, _eventLoop, _http);
-	_eventLoop->addHandler(fd, hanlder);
-	std::cout << "client (" << fd << ") connected." << std::endl; // TODO : insert in the log
+
+	port = ntohs(addr.sin_port);
+	ip = std::string(reinterpret_cast<const char*>(&addr.sin_addr.s_addr), 4);
+	client = new ClientHandler(fd, port, ip, _server, _factory);
+	std::cout << "client connected: " << ip << ":" << port << std::endl;
+
+	flags = fcntl(fd, F_GETFL, 0);
+	if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
+		std::cerr << "fcntl" << std::endl; // TODO: create exceptio
+		return ;
+	}
+
+	_server->addHandler(fd, EPOLLIN, client);
 }
